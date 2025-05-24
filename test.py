94ad42d3f0460abecfa4ae18becdc5a444921324
Ryan McMahon <rmcmahon75@gmail.com>
2025-05-24 16:03:22 -0400

    more work

--- test.py
+++ test.py
@@ -2,16 +2,21 @@ import asyncio
 from logging import basicConfig, INFO, getLogger
 import time
 
-from core.ib_driver import IBDriver
+from core.ib_driver import IBDriver, BarSize
+
 
 async def main():
     logger = getLogger(__name__)
     basicConfig(filename='test.log', level=INFO)
     logger.info("Logging should work")
     ib_driver = IBDriver(sim_account=True, client_id=12)
-    ib_driver.connect()
+    try:
+        ib_driver.connect()
 
-    await ib_driver.get_historical_data("SPY", 10)
+        await ib_driver.get_historical_data("SPY", 10)
+        await ib_driver.get_historical_data("AAPL", 32, bar_size=BarSize.ONE_HOUR)
+    except Exception as ex:
+        print(f"Exception: {ex}")
 
     ib_driver.disconnect()
 
